1451e3b6dc4f0a010d95e66e72c6df755a5a0fac
davidkslack <davidkslack@yahoo.co.uk>
2018-08-31 12:47:19 +0100

    Admin - added manage menu

--- app/Http/Controllers/Menu.php
+++ app/Http/Controllers/Menu.php
@@ -1,12 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+use \Database\Model\Food\Food as Food;
+use \Database\Model\Food\Category as Category;
 
 class Menu extends Controller
 {
 	public function index()
 	{
-		$data = ['name' => 'menu'];
+		$data = [
+			'name' 		=> 'menu',
+			'food' 		=> Food::all()
+		];
+
 		return view('view', $data);
 	}
 }
--- resources/views/content/menu.php
+++ resources/views/content/menu.php
@@ -1,13 +1,30 @@
 <div class="container">
-	<div class="row">
-		<div class="col-sm">
-			One of three columns
-		</div>
-		<div class="col-sm">
-			One of three columns
-		</div>
-		<div class="col-sm">
-			One of three columns
-		</div>
-	</div>
+    <div class="row">
+        <div class="col">
+            <h1>Menu</h1>
+            <p>All food is shown and can be deited from this page.</p>
+
+
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th scope="col">Cost</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($food as $item): ?>
+                    <tr>
+                        <td>£<?php echo number_format($item->cost, 2) ?></td>
+                        <td><?php echo $item->title ?></td>
+                        <td><?php echo $item->description ?></td>
+                        <td><a class="btn btn-primary" href="/admin/menu/<?php echo $item->food_id ?>" role="button">Edit</a></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
