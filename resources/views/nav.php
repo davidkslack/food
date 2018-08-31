9f9b5c1ad87628c994c998351757d276ace022d0
davidkslack <davidkslack@yahoo.co.uk>
2018-08-31 12:11:51 +0100

    Admin - added logo and responsive nav

--- resources/views/nav.php
+++ resources/views/nav.php
@@ -1,5 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<a class="navbar-brand" href="/">Elk Menu</a>
+	<a class="navbar-brand" href="/"><img src="/images/logo224x224.jpg" width="auto" height="40"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto">
 			<li class="nav-item<?php if($name == 'order') echo ' active' ?>">
@@ -14,9 +18,10 @@
 			<li class="nav-item<?php if($name == 'completed') echo ' active' ?>">
 				<a class="nav-link" href="/admin/completed">Completed orders<?php if($name == 'completed') echo ' <span class="sr-only">(current)</span>' ?></a>
 			</li>
-            <li class="nav-item">
-                <a class="nav-link" href="/admin/logout">Logout</a>
-            </li>
 		</ul>
+
+        <span class="align-right">
+            <a class="nav-link" href="/admin/logout" style="padding-left: 0">Logout</a>
+        </span>
 	</div>
 </nav>
